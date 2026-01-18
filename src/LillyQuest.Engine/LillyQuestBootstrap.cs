967b8d1c28b1f85b48f9842e8a59d391c969448a
Tom <squid@stormwind.it>
2026-01-18 09:47:21 +0100

    feat(engine): integrate Silk.NET for windowing and OpenGL support to enhance graphics capabilities feat(engine): implement LillyQuestBootstrap class to manage engine initialization and rendering chore(sln): update project references in solution file for better organization chore(engine): add Silk.NET package references for GLFW and windowing features feat(game): initialize and run LillyQuest engine in the main program to start the application

--- src/LillyQuest.Engine/LillyQuestBootstrap.cs
+++ src/LillyQuest.Engine/LillyQuestBootstrap.cs
@@ -1,21 +1,76 @@
 ﻿using DryIoc;
+using LillyQuest.Core.Data.Configs;
 using LillyQuest.Core.Data.Contexts;
+using LillyQuest.Core.Primitives;
 using Serilog;
+using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
 
 namespace LillyQuest.Engine;
 
 public class LillyQuestBootstrap
 {
-
     private readonly ILogger _logger = Log.ForContext<LillyQuestBootstrap>();
 
     private readonly IContainer _container = new Container();
     private EngineRenderContext _renderContext;
 
-    public void RegisterServices(Func<IContainer,IContainer> registerServices)
+    private readonly GameTime _gameTime = new();
+
+    private readonly LillyQuestEngineConfig _engineConfig;
+
+    private GL _gl;
+    private IWindow _window;
+
+
+    public LillyQuestBootstrap(LillyQuestEngineConfig engineConfig)
+    {
+        _engineConfig = engineConfig;
+        _container.RegisterInstance(_engineConfig);
+    }
+
+    public void RegisterServices(Func<IContainer, IContainer> registerServices) { }
+
+    public void Initialize()
+    {
+        _logger.Information("Initializing LillyQuest Engine...");
+
+        var options = WindowOptions.Default;
+        options.Size = new(_engineConfig.Render.Width, _engineConfig.Render.Height);
+        options.Title = _engineConfig.Render.Title;
+        options.VSync = _engineConfig.Render.IsVSyncEnabled;
+        options.API = new GraphicsAPI(ContextAPI.OpenGL, ContextProfile.Core, ContextFlags.Default, new APIVersion(4, 6));
+
+        _window = Window.Create(options);
+        _window.Update += WindowOnUpdate;
+        _window.Load += WindowOnLoad;
+        _window.Closing += WindowOnClosing;
+        _window.Render += WindowOnRender;
+
+    }
+
+    private void WindowOnRender(double obj)
+    {
+
+    }
+
+    private void WindowOnClosing()
     {
     }
 
+    private void WindowOnLoad()
+    {
+        _gl = GL.GetApi(_window);
 
+    }
 
+    private void WindowOnUpdate(double obj)
+    {
+        _gameTime.Update(obj);
+    }
+
+    public void Run()
+    {
+        _window.Run();
+    }
 }
--- src/LillyQuest.Game/Program.cs
+++ src/LillyQuest.Game/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using LillyQuest.Core.Data.Configs;
+using LillyQuest.Engine;
+
 Console.WriteLine("Hello, World!");
+var lillyQuestBootstrap = new LillyQuestBootstrap(new LillyQuestEngineConfig());
+
+lillyQuestBootstrap.Initialize();
+lillyQuestBootstrap.Run();
