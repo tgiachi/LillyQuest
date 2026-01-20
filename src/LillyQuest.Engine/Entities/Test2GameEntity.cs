4fafa890877f3ef56323a1913c9729a351d78010
tom <squid@stormwind.it>
2026-01-20 15:00:35 +0100

    test: add Test2GameEntity implementation and integrate into TestSceneA
    - Implement Test2GameEntity as a renderable entity that displays "Test2 Entity Rendered"
    - Add Test2GameEntity to TestSceneA during initialization
    - Add necessary using statements and inherit from GameEntity with IRenderableEntity feature

--- src/LillyQuest.Engine/Entities/Test2GameEntity.cs
+++ src/LillyQuest.Engine/Entities/Test2GameEntity.cs
@@ -1,6 +1,22 @@
+using System.Numerics;
+using LillyQuest.Core.Data.Contexts;
+using LillyQuest.Core.Graphics.Rendering2D;
+using LillyQuest.Core.Primitives;
+using LillyQuest.Engine.Interfaces.Features;
+
 namespace LillyQuest.Engine.Entities;
 
-public class Test2GameEntity
+public class Test2GameEntity : GameEntity, IRenderableEntity
 {
-    
+
+    public Test2GameEntity()
+    {
+        Name = "Test2";
+    }
+
+    public void Render(SpriteBatch spriteBatch, EngineRenderContext context)
+    {
+        spriteBatch.DrawFont("default_font", 14, "Test2 Entity Rendered", new Vector2(100,100), LyColor.Red);
+
+    }
 }
--- src/LillyQuest.Game/Scenes/TestSceneA.cs
+++ src/LillyQuest.Game/Scenes/TestSceneA.cs
@@ -26,6 +26,9 @@ public class TestSceneA : BaseScene
                 }
             )
         );
+
+        AddEntity(new Test2GameEntity());
+
         base.OnInitialize(sceneManager);
     }
 }
