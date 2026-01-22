33bd538ccae88d46c682931d875085efc93c3c5f
Tom <squid@stormwind.it>
2026-01-22 20:49:10 +0100

    feat: add UIStackPanel layout container

---
+++ src/LillyQuest.Engine/Screens/UI/UIStackPanel.cs
@@ -0,0 +1,136 @@
+using System.Numerics;
+using LillyQuest.Core.Primitives;
+using Silk.NET.Maths;
+
+namespace LillyQuest.Engine.Screens.UI;
+
+public enum UIStackOrientation
+{
+    Vertical,
+    Horizontal
+}
+
+public enum UICrossAlignment
+{
+    Left,
+    Center,
+    Right
+}
+
+public sealed class UIStackPanel : UIScreenControl
+{
+    private readonly List<UIScreenControl> _children = [];
+    private UIStackOrientation _orientation = UIStackOrientation.Vertical;
+    private UICrossAlignment _crossAxisAlignment = UICrossAlignment.Left;
+    private Vector4 _padding;
+    private float _spacing;
+
+    public IReadOnlyList<UIScreenControl> Children => _children;
+
+    public UIStackOrientation Orientation
+    {
+        get => _orientation;
+        set
+        {
+            _orientation = value;
+            ApplyLayout();
+        }
+    }
+
+    public UICrossAlignment CrossAxisAlignment
+    {
+        get => _crossAxisAlignment;
+        set
+        {
+            _crossAxisAlignment = value;
+            ApplyLayout();
+        }
+    }
+
+    public Vector4 Padding
+    {
+        get => _padding;
+        set
+        {
+            _padding = value;
+            ApplyLayout();
+        }
+    }
+
+    public float Spacing
+    {
+        get => _spacing;
+        set
+        {
+            _spacing = value;
+            ApplyLayout();
+        }
+    }
+
+    public void Add(UIScreenControl control)
+    {
+        if (control == null)
+        {
+            return;
+        }
+
+        control.Parent = this;
+        _children.Add(control);
+        ApplyLayout();
+    }
+
+    public void Remove(UIScreenControl control)
+    {
+        if (control == null)
+        {
+            return;
+        }
+
+        _children.Remove(control);
+        if (control.Parent == this)
+        {
+            control.Parent = null;
+        }
+        ApplyLayout();
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        foreach (var child in _children)
+        {
+            child.Update(gameTime);
+        }
+    }
+
+    private void ApplyLayout()
+    {
+        var cursor = _orientation == UIStackOrientation.Vertical ? _padding.Y : _padding.X;
+        foreach (var child in _children)
+        {
+            if (_orientation == UIStackOrientation.Vertical)
+            {
+                var x = ComputeCrossAxisPosition(Size.X, child.Size.X, _padding.X, _padding.Z);
+                child.Position = new Vector2(x, cursor);
+                cursor += child.Size.Y + _spacing;
+            }
+            else
+            {
+                var y = ComputeCrossAxisPosition(Size.Y, child.Size.Y, _padding.Y, _padding.W);
+                child.Position = new Vector2(cursor, y);
+                cursor += child.Size.X + _spacing;
+            }
+        }
+    }
+
+    private float ComputeCrossAxisPosition(float total, float child, float startPad, float endPad)
+    {
+        var available = total - startPad - endPad;
+        return _crossAxisAlignment switch
+        {
+            UICrossAlignment.Left => startPad,
+            UICrossAlignment.Center => startPad + (available - child) * 0.5f,
+            UICrossAlignment.Right => startPad + (available - child),
+            _ => startPad
+        };
+    }
+}
---
+++ tests/LillyQuest.Tests/Engine/UI/UIStackPanelTests.cs
@@ -0,0 +1,70 @@
+using System.Numerics;
+using LillyQuest.Engine.Screens.UI;
+using Silk.NET.Maths;
+
+namespace LillyQuest.Tests.Engine.UI;
+
+public class UIStackPanelTests
+{
+    [Test]
+    public void VerticalLayout_Uses_Padding_And_Spacing()
+    {
+        var panel = new UIStackPanel
+        {
+            Size = new Vector2(100, 100),
+            Orientation = UIStackOrientation.Vertical,
+            Spacing = 4f,
+            Padding = new Vector4(10, 5, 10, 5),
+            CrossAxisAlignment = UICrossAlignment.Left
+        };
+
+        var a = new UIScreenControl { Size = new Vector2(20, 10) };
+        var b = new UIScreenControl { Size = new Vector2(20, 10) };
+        panel.Add(a);
+        panel.Add(b);
+
+        Assert.That(a.Position, Is.EqualTo(new Vector2(10, 5)));
+        Assert.That(b.Position, Is.EqualTo(new Vector2(10, 5 + 10 + 4)));
+    }
+
+    [Test]
+    public void HorizontalLayout_Centers_On_CrossAxis()
+    {
+        var panel = new UIStackPanel
+        {
+            Size = new Vector2(100, 50),
+            Orientation = UIStackOrientation.Horizontal,
+            Spacing = 5f,
+            Padding = new Vector4(10, 5, 10, 5),
+            CrossAxisAlignment = UICrossAlignment.Center
+        };
+
+        var a = new UIScreenControl { Size = new Vector2(20, 10) };
+        panel.Add(a);
+
+        var availableHeight = 50 - 5 - 5;
+        var expectedY = 5 + (availableHeight - 10) * 0.5f;
+
+        Assert.That(a.Position, Is.EqualTo(new Vector2(10, expectedY)));
+    }
+
+    [Test]
+    public void VerticalLayout_Right_Aligns_Children()
+    {
+        var panel = new UIStackPanel
+        {
+            Size = new Vector2(100, 50),
+            Orientation = UIStackOrientation.Vertical,
+            Padding = new Vector4(10, 5, 10, 5),
+            CrossAxisAlignment = UICrossAlignment.Right
+        };
+
+        var a = new UIScreenControl { Size = new Vector2(20, 10) };
+        panel.Add(a);
+
+        var availableWidth = 100 - 10 - 10;
+        var expectedX = 10 + (availableWidth - 20);
+
+        Assert.That(a.Position, Is.EqualTo(new Vector2(expectedX, 5)));
+    }
+}
