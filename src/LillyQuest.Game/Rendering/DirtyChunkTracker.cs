f8dc4cdaf4a8641804035ea6e12b093c9c6c7343
tom <squid@stormwind.it>
2026-01-28 10:49:58 +0100

    feat: track dirty chunks

--- src/LillyQuest.Game/Rendering/DirtyChunkTracker.cs
+++ src/LillyQuest.Game/Rendering/DirtyChunkTracker.cs
@@ -16,6 +16,11 @@ public class DirtyChunkTracker
         _chunkSize = chunkSize;
     }
 
+    public HashSet<ChunkCoord> DirtyChunks { get; } = new();
+
     public ChunkCoord GetChunkCoord(int x, int y)
         => new(x / _chunkSize, y / _chunkSize);
+
+    public void MarkDirtyForTile(int x, int y)
+        => DirtyChunks.Add(GetChunkCoord(x, y));
 }
--- tests/LillyQuest.Tests/Game/Rendering/DirtyChunkTrackerTests.cs
+++ tests/LillyQuest.Tests/Game/Rendering/DirtyChunkTrackerTests.cs
@@ -15,4 +15,14 @@ public class DirtyChunkTrackerTests
         Assert.That(tracker.GetChunkCoord(32, 0), Is.EqualTo(new ChunkCoord(1, 0)));
         Assert.That(tracker.GetChunkCoord(0, 32), Is.EqualTo(new ChunkCoord(0, 1)));
     }
+
+    [Test]
+    public void MarkDirtyForTile_AddsChunk()
+    {
+        var tracker = new DirtyChunkTracker(chunkSize: 16);
+
+        tracker.MarkDirtyForTile(17, 0);
+
+        Assert.That(tracker.DirtyChunks, Does.Contain(new ChunkCoord(1, 0)));
+    }
 }
